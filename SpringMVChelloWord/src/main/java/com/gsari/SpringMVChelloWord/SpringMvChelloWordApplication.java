b8baa64553d3b76a1eac6993e9a5ca40b17af5c4
GamzeK <gkrc094@gmail.com>
2022-11-24 14:56:54 +0300

    first..

---
+++ SpringMVChelloWord/src/main/java/com/gsari/SpringMVChelloWord/SpringMvChelloWordApplication.java
@@ -0,0 +1,13 @@
+package com.gsari.SpringMVChelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvChelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvChelloWordApplication.class, args);
+	}
+
+}
